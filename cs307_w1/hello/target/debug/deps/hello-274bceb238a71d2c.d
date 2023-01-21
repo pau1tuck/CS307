7e1c49923cb7df47d0adee601418b4e1ef5160d6
Paul Tuck <paul.tuck@oxon.org>
2023-01-21 18:41:13 +0700

    "2023-01-21_18:41_ICT+7"

---
+++ cs307_w1/hello/target/debug/deps/hello-274bceb238a71d2c.d
@@ -0,0 +1,5 @@
+/home/paul/wsl/dev/CS307/cs307_w1/hello/target/debug/deps/hello-274bceb238a71d2c: src/main.rs
+
+/home/paul/wsl/dev/CS307/cs307_w1/hello/target/debug/deps/hello-274bceb238a71d2c.d: src/main.rs
+
+src/main.rs:
---
+++ cs307_w1/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/paul/wsl/dev/CS307/cs307_w1/hello/target/debug/hello: /home/paul/wsl/dev/CS307/cs307_w1/hello/src/main.rs
