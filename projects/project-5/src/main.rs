dac39ca0d32cc30399d6ef0a0a0ef75c35fdb5ce
Paul Tuck <paul.tuck@oxon.org>
2023-02-02 00:49:42 +0700

    "2023-02-02_00:49_ICT+7"

--- projects/project-5/src/main.rs
+++ projects/project-5/src/main.rs
@@ -1,9 +1,10 @@
 extern crate dotenv;
 use dotenv::dotenv;
 use reqwest::Client;
+use serde::Deserialize;
 use std::env;
 
-#[derive(Debug, serde::Deserialize)]
+#[derive(Debug, Deserialize)]
 struct Weather {
     temperature: String,
 }
@@ -11,13 +12,10 @@ struct Weather {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok(); // This line loads the environment variables from the ".env" file.
     let api_key = std::env::var("API_KEY").expect("API_KEY must be defined.");
-    let address = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Mae%20Chan?unitGroup=metric&include=current&key=" + api_key + "&contentType=json"
+    let address = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Mae%20Chan?unitGroup=metric&include=current&key=" + api_key + "&contentType=json";
 
     let client = Client::new();
-    let response = client
-        .get(address)
-        .send()?
-        .json::<Weather>()?;
+    let response = client.get(address).send()?.json::<Weather>()?;
 
     println!("{:?}", response);
     Ok(())
