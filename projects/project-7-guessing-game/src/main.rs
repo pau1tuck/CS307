d79d801a1524079a3b4f15e911ec902ef11e1dcd
Paul Tuck <paul.tuck@oxon.org>
2023-02-05 16:26:16 +0700

    "2023-02-05_16:26_ICT+7"

--- projects/project-7-guessing-game/src/main.rs
+++ projects/project-7-guessing-game/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Try to guess the number!");
+
+    println!("Please enter your guess.");
+
+    let mut guess = String::new();
+    // An associated function is a function that’s implemented on a type, in this case String.
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
