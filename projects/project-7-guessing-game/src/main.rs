5e2f224937a722ce9e3286bef28c23014ae7c282
Paul Tuck <paul.tuck@oxon.org>
2023-02-06 14:50:59 +0700

    "2023-02-06_14:50_ICT+7"

--- projects/project-7-guessing-game/src/main.rs
+++ projects/project-7-guessing-game/src/main.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+// The 'Rng trait' defines methods that random number generators implement, and this trait must be in scope for us to use those methods.
 use std::io;
 
 fn main() {
@@ -10,7 +12,12 @@ fn main() {
 
     io::stdin()
         .read_line(&mut guess)
+        // read_line puts whatever the user enters into the string we pass to it, but it also returns a Result value.
         .expect("Failed to read line");
+
+    /* Result is an enumeration, often called an enum, which is a type that can be in one of multiple possible states.
+    We call each possible state a 'variant'.
+    The purpose of these Result types is to encode error-handling information. */
 
     println!("You guessed: {guess}");
 }
