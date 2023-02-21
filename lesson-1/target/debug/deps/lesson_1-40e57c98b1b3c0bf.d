3da137875410e6cf9faa91296d788ca3ed7f13b2
Paul Tuck <paul.tuck@oxon.org>
2023-02-21 19:59:35 +0700

    "2023-02-21_19:59_ICT+7"

--- lesson-1/target/debug/deps/lesson_1-40e57c98b1b3c0bf.d
+++ lesson-1/target/debug/deps/lesson_1-40e57c98b1b3c0bf.d
@@ -1,5 +1,5 @@
-/home/paul/dev/training/CS307/lesson-1/target/debug/deps/lesson_1-40e57c98b1b3c0bf.rmeta: src/main.rs
+/home/paul/dev/CS307/lesson-1/target/debug/deps/lesson_1-40e57c98b1b3c0bf.rmeta: src/main.rs
 
-/home/paul/dev/training/CS307/lesson-1/target/debug/deps/lesson_1-40e57c98b1b3c0bf.d: src/main.rs
+/home/paul/dev/CS307/lesson-1/target/debug/deps/lesson_1-40e57c98b1b3c0bf.d: src/main.rs
 
 src/main.rs:
