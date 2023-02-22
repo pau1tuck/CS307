b19e5dac8ea0c494e12862f971cbeb4501171be8
Paul Tuck <paul.tuck@oxon.org>
2023-02-22 15:56:39 +0700

    "2023-02-22_15:56_ICT+7"

--- project-8-mars/src/main.rs
+++ project-8-mars/src/main.rs
@@ -3,8 +3,8 @@
 fn main() {
     println!("Number: {}, String: {}", 100, "abcde"); // `println!()` is a macro that can receive a variable number of arguments.
                                                       // To learn more: `cargo install cargo-expand`, then `run cargo expand`.
-
-    let mars_weight = calculate_weight_on_mars(90.0); // Compiler infers `weight: f32` from function argument
+                                                      // The `mut` keyword makes our new variable mutable (changeable):
+    let mut mars_weight: f32 = calculate_weight_on_mars(90.0); // Compiler infers `weight: f32` from the function argument.
     println!(
         "Weight on Mars: {} kg",
         calculate_weight_on_mars(mars_weight)
