f36ba35fb6231600c8986af0ebe3676d1b080c08
Paul Tuck <paul.tuck@oxon.org>
2023-02-22 15:38:48 +0700

    "2023-02-22_15:38_ICT+7"

--- project-8-mars/src/main.rs
+++ project-8-mars/src/main.rs
@@ -4,9 +4,13 @@ fn main() {
     println!("Number: {}, String: {}", 100, "abcde"); // `println!()` is a macro that can receive a variable number of arguments.
                                                       // To learn more: `cargo install cargo-expand`, then `run cargo expand`.
 
-    println!("Weight on Mars: {} kg", calculate_weight_on_mars(100.0));
+    let mars_weight = calculate_weight_on_mars(90.0); // Compiler infers `weight: f32` from function argument
+    println!(
+        "Weight on Mars: {} kg",
+        calculate_weight_on_mars(mars_weight)
+    );
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
-    (weight / 9.81) & 3.711 // No semicolon on return expression!
+    (weight / 9.81) * 3.711 // Don't add a semicolon to your return expression!
 }
