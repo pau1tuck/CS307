fb846989831f7417143f4ccb30abcafe910a5a29
Paul Tuck <paul.tuck@oxon.org>
2023-02-22 15:06:30 +0700

    "2023-02-22_15:06_ICT+7"

--- project-6-datetime/src/main.rs
+++ project-6-datetime/src/main.rs
@@ -1,12 +1,26 @@
-use chrono::prelude::*;
+use chrono::{DateTime, Local};
 
 fn main() {
-    let months = [
-        "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
-    ];
     let local: DateTime<Local> = Local::now();
-    let month: &str = months[local.month()];
-    println!("{}-{}", local.month(), local.year());
+    let day = local.format("%a");
+    let date = local.format("%e");
+    let month = local.format("%b");
+    let year = local.format("%Y");
+    let hour = local.format("%H");
+    let minute = local.format("%M");
+
+    println!("{} {} {} {}, {}:{}", day, date, month, year, hour, minute);
 }
 
-// months[local.month() - 1]
+/*
+use chrono::{DateTime, Utc};
+
+fn main() {
+    let local: DateTime<Utc> = Utc::local();
+
+    println!("UTC local is: {}", local);
+    println!("UTC local in RFC 2822 is: {}", local.to_rfc2822());
+    println!("UTC local in RFC 3339 is: {}", local.to_rfc3339());
+    println!("UTC local in a custom format is: {}", local.format("%a %b %e %T %Y"));
+}
+ */
